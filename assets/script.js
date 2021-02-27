2f5b8601b871e97a48c1c2fe68854dfaeeda7db8
TMats <takomats@hotmail.com>
2021-02-27 12:50:48 -0500

    Foundation Added

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+// make a timer that counting from 10 - 0
+// it click it to 0 - shows a prompt on the screen saying "play again?"
+// Fernando works on the top 2, [Timer & clicker prompt]
+// _ _ _ _ _ : Hidden Word Function
+// Takuya works on Hidden Word Function
+// Win & Lose Counter, It was work with the Hidden Word Function
+// Mina works on Win & Lose Counter
+// Start button last function to communicate with the other functions. 
